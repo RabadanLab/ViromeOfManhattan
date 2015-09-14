1909ed1ffa27321ea1dd70ce7567eead00994e4a
sakellarios zairis <szairis@gmail.com>
2015-09-14 13:40:02 -0400

    modified path to mitcr.jar in tcr module script

--- tcr_repertoire.sh
+++ tcr_repertoire.sh
@@ -12,9 +12,9 @@ mkdir tcr
 echo zcat [ `date` ] 
 zcat $1 $2 > tcr/both_mates.fastq
 echo alpha chain [ `date` ] 
-java -Xmx16g -jar /ifs/home/c2b2/rr_lab/siz2102/apps/Pandora/mitcr.jar -pset flex -level 3 -gene TRA tcr/both_mates.fastq tcr/alpha_chain.txt
+java -Xmx16g -jar /ifs/home/c2b2/rr_lab/siz2102/apps/Pandora/resources/mitcr.jar -pset flex -level 3 -gene TRA tcr/both_mates.fastq tcr/alpha_chain.txt
 echo beta chain [ `date` ] 
-java -Xmx16g -jar /ifs/home/c2b2/rr_lab/siz2102/apps/Pandora/mitcr.jar -pset flex -level 3 -gene TRB tcr/both_mates.fastq tcr/beta_chain.txt
+java -Xmx16g -jar /ifs/home/c2b2/rr_lab/siz2102/apps/Pandora/resources/mitcr.jar -pset flex -level 3 -gene TRB tcr/both_mates.fastq tcr/beta_chain.txt
 rm tcr/both_mates.fastq
 
 echo TCR CLONALITY END [[ `date` ]]
